bc5abc869b4bd17cd12d4d27c9d4df1fba6949dc
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-21 21:25:04 +0530

    Add Token for all operations

--- src/service/BookData.ts
+++ src/service/BookData.ts
@@ -2,64 +2,90 @@ import axios from 'axios'
 
 const baseURL = "http://localhost:8082/booklib/api/v1/books"
 
-const AddBookData = async(book :any) =>{
+const fetchToken = () => {
+  const token = localStorage.getItem("libToken")
+  return "Bearer " + token
+}
+
+const AddBookData = async (book: any) => {
   //  save a book
-  console.log("Save book.......................................",book)
-  try{
+  console.log("Save book.......................................", book)
+  try {
     const response = await axios.post(
-        baseURL,
-        book
-      );
+      baseURL,
+      book, {
+      headers: {
+        Authorization: fetchToken()
+      }
+    }
+    );
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to get the data",error);
-      throw error
+
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
   }
 }
 
 
-const DeleteBook = async(bookId :String) =>{
+const DeleteBook = async (bookId: String) => {
   //  get the books
-  try{
+  try {
     console.log(bookId)
-    const response  =  await axios.delete(
-        `${baseURL}?bookId=${bookId}`);
+    const response = await axios.delete(
+      `${baseURL}?bookId=${bookId}`,
+      {
+        headers: {
+          Authorization: fetchToken()
+        }
+      }
+
+    );
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to delete the data",error);
-      throw error
+
+  } catch (error) {
+    console.error("Failed to delete the data", error);
+    throw error
   }
 }
 
 
 const GetBooks = async () => {
-   //get the books
-      try{
-        const response = await  axios.get(`${baseURL}/getAllbooks`)
-        return response.data
-      }catch(error){
-        console.error("Failed to get the data",error);
-        throw error
+  //get the books
+  try {
+    const response = await axios.get(`${baseURL}/getAllbooks`,
+      {
+        headers: {
+          Authorization: fetchToken()
+        }
       }
+    );
+    return response.data
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
+  }
 }
 
-const UpdateBook = async(book :any) =>{
+const UpdateBook = async (book: any) => {
   //  get the books
-  try{
-    const response  =  await axios.patch(
-        `${baseURL}?bookId=${book.bookId}`,
-        book
-        );
+  try {
+    const response = await axios.patch(
+      `${baseURL}?bookId=${book.bookId}`,
+      book,{
+          headers:{
+            Authorization: fetchToken()
+          }
+        }
+    );
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to get the data",error);
-      throw error
-  }   
+
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
+  }
 }
-export {AddBookData, DeleteBook, GetBooks, UpdateBook}
+export { AddBookData, DeleteBook, GetBooks, UpdateBook }
