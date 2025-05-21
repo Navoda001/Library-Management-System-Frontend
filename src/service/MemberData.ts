7e37f7ada6e7df298e358903f2dc38064aa3ccc6
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-21 22:46:20 +0530

    add auth

--- src/service/MemberData.ts
+++ src/service/MemberData.ts
@@ -2,64 +2,85 @@ import axios from 'axios'
 
 const baseURL = "http://localhost:8082/booklib/api/v1/members"
 
-const AddMemberData = async(member :any) =>{
+const fetchToken = () => {
+  const token = localStorage.getItem("libToken")
+  return "Bearer " + token
+}
+
+const AddMemberData = async (member: any) => {
   //  save a book
-  console.log("Save Member.......................................",member)
-  try{
+  console.log("Save Member.......................................", member)
+  try {
     const response = await axios.post(
-        baseURL,
-        member
-      );
+      baseURL,
+      member, {
+      headers: {
+        Authorization: fetchToken()
+      }
+    }
+    );
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to get the data",error);
-      throw error
+
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
   }
 }
 
 
-const DeleteMember = async(memberId :String) =>{
+const DeleteMember = async (memberId: String) => {
   //  get the books
-  try{
+  try {
     console.log(memberId)
-    const response  =  await axios.delete(
-        `${baseURL}?memberId=${memberId}`);
+    const response = await axios.delete(
+      `${baseURL}?memberId=${memberId}`, {
+      headers: {
+        Authorization: fetchToken()
+      }
+    });
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to delete the data",error);
-      throw error
+
+  } catch (error) {
+    console.error("Failed to delete the data", error);
+    throw error
   }
 }
 
 
 const GetMembers = async () => {
-   //get the books
-      try{
-        const response = await  axios.get(`${baseURL}/getAllMember`)
-        return response.data
-      }catch(error){
-        console.error("Failed to get the data",error);
-        throw error
+  //get the books
+  try {
+    const response = await axios.get(`${baseURL}/getAllMember`, {
+      headers: {
+        Authorization: fetchToken()
       }
+    });
+    return response.data
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
+  }
 }
 
-const UpdateMember = async(member :any) =>{
+const UpdateMember = async (member: any) => {
   //  get the books
-  try{
-    const response  =  await axios.patch(
-        `${baseURL}?memberId=${member.memberId}`,
-        member
-        );
+  try {
+    const response = await axios.patch(
+      `${baseURL}?memberId=${member.memberId}`,
+      member, {
+      headers: {
+        Authorization: fetchToken()
+      }
+    }
+    );
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to get the data",error);
-      throw error
-  }   
+
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
+  }
 }
-export {AddMemberData, DeleteMember, GetMembers, UpdateMember}
+export { AddMemberData, DeleteMember, GetMembers, UpdateMember }
--- src/service/StaffData.ts
+++ src/service/StaffData.ts
@@ -2,64 +2,86 @@ import axios from 'axios'
 
 const baseURL = "http://localhost:8082/booklib/api/v1/staff"
 
-const AddStaffData = async(staff :any) =>{
+const fetchToken = () => {
+  const token = localStorage.getItem("libToken")
+  return "Bearer " + token
+}
+
+const AddStaffData = async (staff: any) => {
   //  save a staff member
-  console.log("Save Staff Member.......................................",staff)
-  try{
+  console.log("Save Staff Member.......................................", staff)
+  try {
     const response = await axios.post(
-        baseURL,
-        staff
-      );
+      baseURL,
+      staff, {
+      headers: {
+        Authorization: fetchToken()
+      }
+    }
+    );
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to get the data",error);
-      throw error
+
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
   }
 }
 
 
-const DeleteStaff = async(staffId :String) =>{
+const DeleteStaff = async (staffId: String) => {
   //  get the staff member
-  try{
+  try {
     console.log(staffId)
-    const response  =  await axios.delete(
-        `${baseURL}?staffId=${staffId}`);
+    const response = await axios.delete(
+      `${baseURL}?staffId=${staffId}`, {
+      headers: {
+        Authorization: fetchToken()
+      }
+    });
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to delete the data",error);
-      throw error
+
+  } catch (error) {
+    console.error("Failed to delete the data", error);
+    throw error
   }
 }
 
 
 const GetStaff = async () => {
-   //get the staff
-      try{
-        const response = await  axios.get(`${baseURL}/getAllStaff`)
-        return response.data
-      }catch(error){
-        console.error("Failed to get the data",error);
-        throw error
+  //get the staff
+  try {
+    const response = await axios.get(`${baseURL}/getAllStaff`, {
+      headers: {
+        Authorization: fetchToken()
       }
+    });
+    return response.data
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
+  }
 }
 
-const UpdateStaff = async(staff :any) =>{
+const UpdateStaff = async (staff: any) => {
   //  get the books
-  try{
-    const response  =  await axios.patch(
-        `${baseURL}?staffId=${staff.staffId}`,
-        staff
-        );
+  try {
+    const response = await axios.patch(
+      `${baseURL}?staffId=${staff.staffId}`,
+      staff,
+      {
+        headers: {
+          Authorization: fetchToken()
+        }
+      }
+    );
     console.log(response.data)
     return response.data;
-    
-  }catch(error){
-      console.error("Failed to get the data",error);
-      throw error
-  }   
+
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error
+  }
 }
-export {AddStaffData, DeleteStaff, GetStaff, UpdateStaff}
+export { AddStaffData, DeleteStaff, GetStaff, UpdateStaff }
