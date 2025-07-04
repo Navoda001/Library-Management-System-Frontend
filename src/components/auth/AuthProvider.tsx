fc02296ebea6916bcbd69dd00d911ab6d38397a5
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-07-04 16:49:34 +0530

    update Auth

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -1,49 +1,59 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { jwtDecode } from 'jwt-decode'
 
 interface AuthContextType {
-    isAuthenticated:boolean;
-    login:(token:string) => void;
-    logout:() => void;
+    isAuthenticated: boolean;
+    login: (token: string) => void;
+    logout: () => void;
+}
+
+interface JwtPayload {
+  sub: string;
+  roles: string;
+  iat: number;
+  exp: number;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 
-export const AuthProvider = ({children} :{ children:ReactNode}) =>{
-   
-    const [ isAuthenticated , setisAuthenticated ] = useState(false)
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
-      useEffect( ()=>{
+    const [isAuthenticated, setisAuthenticated] = useState(false)
+
+    useEffect(() => {
         //get the token from local strage and validate
         const token = localStorage.getItem("libToken")
 
-         if(token){
+        if (token) {
             setisAuthenticated(!!token)
         }
-    },[])
+    }, [])
 
-    const login = (token:string)=>{
+    const login = (token: string) => {
         //set token from localstorage
-        localStorage.setItem("libToken",token)
+        localStorage.setItem("libToken", token)
+        const decoded = jwtDecode<JwtPayload>(token);
+        console.log(decoded);
         setisAuthenticated(true)
     }
-    const logout = ()=>{
+    const logout = () => {
         //remove token from localstorage 
         localStorage.removeItem("libToken")
         setisAuthenticated(false)
     }
 
 
-   return(
-      <AuthContext.Provider value = {{isAuthenticated, login, logout}}>
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
             {children}
-      </AuthContext.Provider>
-   );
+        </AuthContext.Provider>
+    );
 }
 
-export const useAuth = () =>{
+export const useAuth = () => {
     const context = useContext(AuthContext)
-    if(!context){
+    if (!context) {
         throw new Error("useAuth should be used within an AuthProvider")
     }
     return context;
